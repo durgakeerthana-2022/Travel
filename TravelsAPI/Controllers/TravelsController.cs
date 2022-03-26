14cec6f4a3cb97b35385347265ae98287cab5bad
durgakeerthana-2022 <102024907+durgakeerthana-2022@users.noreply.github.com>
2022-03-26 17:30:23 +0530

    Add files via upload

---
+++ TravelsAPI/Controllers/TravelsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelsAPI.Models;
+
+namespace TravelsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TravelsController : ControllerBase
+    {
+        private readonly TravelDBContext _context;
+
+        public TravelsController(TravelDBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Travels
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Travels>>> GetTravels()
+        {
+            return await _context.TravelDetails.ToListAsync();
+        }
+
+        // GET: api/Travels/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Travels>> GetTravels(int id)
+        {
+            var travels = await _context.Travels.FindAsync(id);
+
+            if (travels == null)
+            {
+                return NotFound();
+            }
+
+            return travelDetail;
+        }
+
+        // PUT: api/Travels/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTravels(int id, Travels travelDetail)
+        {
+            if (id != travels.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(travels).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TravelsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Travels
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Travels>> PostTravels(Travels travels)
+        {
+            _context.Travels.Add(travels);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTravels", new { id = travels.Id }, travels);
+        }
+
+        // DELETE: api/Travels/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTravels(int id)
+        {
+            var travels = await _context.Travels.FindAsync(id);
+            if (travels == null)
+            {
+                return NotFound();
+            }
+
+            _context.Travels.Remove(travels);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TravelsExists(int id)
+        {
+            return _context.Travels.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TravelsAPI/Models/TravelDBContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TravelsAPI.Models
+{
+    public partial class TravelDBContext : DbContext
+    {
+        public TravelDBContext()
+        {
+        }
+
+        public TravelDBContext(DbContextOptions<TravelDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Travels> Travels { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Travels>(entity =>
+            {
+                entity.Property(e => e.Contact).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Place).HasMaxLength(150);
+
+                entity.Property(e => e.Email).HasMaxLength(50);
+
+               
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
